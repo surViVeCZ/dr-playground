6c86808b4f207d915f503a3ebe66ced9c9665e9a
survivecz <poucpetr11@seznam.cz>
2023-10-28 19:39:02 +0200

    alive, vt check

--- livetest.sh
+++ livetest.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-# Define the file with the list of domains
-DOMAINS_FILE=$1
-
-# Check if the file exists
-if [[ ! -f "$DOMAINS_FILE" ]]; then
-    echo "File not found: $DOMAINS_FILE"
-    exit 1
+# Ping the domain and check if it responds
+if ping -c 1 -W 1 $1 > /dev/null 2>&1
+then
+    # Domain is alive
+    echo 1
+else
+    # Domain is not alive
+    echo 0
 fi
-
-# Function to check if a domain is live
-check_domain() {
-    domain=$1
-    # Remove carriage return '\r'
-    domain=${domain//$'\r'/}
-    # Ping the domain with a timeout of 5 seconds
-    # Only one ping packet is sent to make the script faster
-    if ping -c 1 -W 5 "$domain" > /dev/null 2>&1; then
-        echo "$domain"
-    fi
-}
-
-# Read each domain from the file and check if it's live
-while IFS= read -r domain || [[ -n "$domain" ]]; do
-    # Pass the domain to the check function
-    check_domain "$domain"
-done < "$DOMAINS_FILE"
--- virus_total.py
+++ virus_total.py
@@ -1,16 +1,42 @@
+import os
+import subprocess
+from typing import List, Tuple, Optional
+
 import vt
-from tqdm import tqdm
 import pandas as pd
 import matplotlib.pyplot as plt
-from typing import List, Tuple, Optional
+from tqdm import tqdm
+from dotenv import load_dotenv
 
 
 class DomainAnalyzer:
-    def __init__(self, api_key: str):
+    def __init__(self):
         """
-        Initializes the DomainAnalyzer with a VirusTotal API key.
+        Initializes the DomainAnalyzer with a VirusTotal API key loaded from an environment variable.
         """
-        self.client = vt.Client(api_key)
+        self.client = self.initialize_client()
+
+    @staticmethod
+    def initialize_client():
+        """
+        Load API key and initialize the vt.Client.
+        """
+        load_dotenv()  # Load environment variables from .env file
+        api_key = os.getenv('VT_API_KEY')  # Get the API key from the environment variable
+
+        if api_key is None:
+            raise ValueError("API key is not set. Please set the VT_API_KEY environment variable.")
+
+        return vt.Client(api_key)
+        
+    @staticmethod
+    def read_domains_from_file(filename: str) -> List[str]:
+        """
+        Read domains from a given file.
+        """
+        with open(filename, "r") as file:
+            lines = file.readlines()
+        return [line.split(",")[0].split(": ")[1] for line in lines]
 
     def check_domain(self, domain: str) -> Optional[vt.Object]:
         """
@@ -19,7 +45,6 @@ class DomainAnalyzer:
         try:
             url_id = vt.url_id(domain)
             obj = self.client.get_object(f"/urls/{url_id}")
-            self.client.close()
             return obj
         except vt.APIError as e:
             print(f"Error: Unable to fetch information for domain {domain}. {e}")
@@ -29,19 +54,10 @@ class DomainAnalyzer:
         """
         Determine the verdict of the analysis.
         """
-        if analysis_stats.get('malicious', 0) > 0 or analysis_stats.get('suspicious', 0) > 0:
+        if analysis_stats.get('malicious', 0) > 0 or analysis_stats.get('suspicious', 0) > 1:
             return "Malign"
         else:
             return "Benign"
-
-    @staticmethod
-    def read_domains_from_file(filename: str) -> List[str]:
-        """
-        Read domains from a given file.
-        """
-        with open(filename, "r") as file:
-            lines = file.readlines()
-        return [line.split(",")[0].split(": ")[1] for line in lines]
 
     def process_domains(self, filename: str) -> pd.DataFrame:
         """
@@ -53,43 +69,69 @@ class DomainAnalyzer:
         for domain in tqdm(domains, desc="Processing domains", unit="domain"):
             result = self.check_domain(domain)
             if result:
-                analysis_stats = result.last_analysis_stats
-                verdict = self.get_verdict(analysis_stats)
-                detection_ratio = f"{analysis_stats['malicious']}/{analysis_stats['malicious'] + analysis_stats['harmless']}"
-                detection_timestamp = result.last_analysis_date
+                data.append(self.extract_domain_data(domain, result))
 
-                data.append([domain, verdict, detection_ratio, detection_timestamp, analysis_stats.get('harmless', 0),
-                             analysis_stats.get('malicious', 0), analysis_stats.get('suspicious', 0)])
+        columns = ["Domain", "Verdict", "Detection Ratio", "Detection Timestamp", "Harmless", "Malicious", "Suspicious", "Live Status"]
+        df = pd.DataFrame(data, columns=columns)
 
-        df = pd.DataFrame(data, columns=["Domain", "Verdict", "Detection Ratio", "Detection Timestamp", "Harmless", "Malicious", "Suspicious"])
-
+        # Sorting the DataFrame by Verdict and Live Status columns
+        df.sort_values(by=['Verdict', 'Live Status'], ascending=[False, False], inplace=True)
+        
         return df
+
+    def is_domain_live(self, domain: str) -> str:
+        """
+        Check if a domain is live by calling a bash script.
+        """
+        try:
+            # Running the bash script and capturing the output
+            result = subprocess.run(['./livetest.sh', domain], capture_output=True, text=True)
+            output = result.stdout.strip()
+            if output == '1':
+                return "Alive"
+            else:
+                return "Dead"
+        except Exception as e:
+            print(f"Error: Unable to check if domain {domain} is live. {e}")
+            return "Unknown"
+        
+    def extract_domain_data(self, domain: str, result: vt.Object) -> Tuple:
+        """
+        Extract necessary data from the domain result.
+        """
+        analysis_stats = result.last_analysis_stats
+        verdict = self.get_verdict(analysis_stats)
+        detection_ratio = f"{analysis_stats['malicious']}/{analysis_stats['malicious'] + analysis_stats['harmless']}"
+        detection_timestamp = result.last_analysis_date
+        domain_status = self.is_domain_live(domain)
+        return domain, verdict, detection_ratio, detection_timestamp, analysis_stats.get('harmless', 0), \
+               analysis_stats.get('malicious', 0), analysis_stats.get('suspicious', 0), domain_status
 
     def generate_report(self, df: pd.DataFrame, output_filename: str) -> None:
         """
         Generate a report based on the DataFrame and save it as a PDF.
         """
-        # Generate a table from the DataFrame
-        fig, ax = plt.subplots(figsize=(12, 4))
-        ax.xaxis.set_visible(False)  
-        ax.yaxis.set_visible(False)  
-        ax.set_frame_on(False)
-        
-        colWidths = [max(df["Domain"].apply(len)*0.7) * 0.02 if column == "Domain" else 0.15 for column in df.columns]
-        
-        tab = pd.plotting.table(ax, df, loc='center', colWidths=colWidths, cellLoc='center', rowLoc='center')
-        tab.auto_set_font_size(True) 
-        tab.set_fontsize(8)  
-        tab.scale(1.2, 1.2)
-        
-        # Adding benign and malign counts as text above the table
+
         benign_count = len(df[df['Verdict'] == 'Benign'])
         malign_count = len(df[df['Verdict'] == 'Malign'])
         total_count = len(df)
-    
-        header_text = f"Benign count: {benign_count}/{total_count} ({(benign_count/total_count)*100:.2f}%)\nMalign count: {malign_count}/{total_count} ({(malign_count/total_count)*100:.2f}%)"
-        plt.title(header_text, fontsize=10, pad=20)
         
+        benign_row = pd.DataFrame([['', 'Benign count', f'{benign_count}/{total_count}', '', '', '', '', '']], columns=df.columns)
+        malign_row = pd.DataFrame([['', 'Malign count', f'{malign_count}/{total_count}', '', '', '', '', '']], columns=df.columns)
+        
+        df = pd.concat([df, benign_row, malign_row], ignore_index=True)
+        # Adjust the height of the figure based on the number of rows in the DataFrame
+        fig_height = len(df) * 0.2
+        fig, ax = plt.subplots(figsize=(12, fig_height))
+        ax.axis('off')  # Hide axes
+        
+        colWidths = [max(df["Domain"].apply(len)*0.7) * 0.02 if column == "Domain" else 0.15 for column in df.columns]
+        
+        tab = pd.plotting.table(ax, df, loc='upper center', colWidths=colWidths, cellLoc='center', rowLoc='center')
+        tab.auto_set_font_size(True) 
+        tab.set_fontsize(8)  
+        tab.scale(1.2, 1.2)
+
         # Style adjustments (bold headers, colors based on verdict, hiding index)
         for key, cell in tab.get_celld().items():
             if key[0] == 0 or key[1] == -1:
@@ -100,14 +142,20 @@ class DomainAnalyzer:
                 cell._text.set_color('green')
             if key[1] == -1:
                 cell.set_visible(False)
+            if key[0] in [total_count+1, total_count+2]:  # Special styling for the benign and malign count rows
+                cell._text.set_weight('bold')
+                cell.set_facecolor('lightgrey')
+            if cell.get_text().get_text() == 'Dead':
+                cell._text.set_color('red')
+            elif cell.get_text().get_text() == 'Alive':
+                cell._text.set_color('green')
         
         # Save the table as a PDF
         plt.savefig(output_filename, bbox_inches='tight', dpi=300)
         plt.close()
 
-
-
 if __name__ == "__main__":
-    analyzer = DomainAnalyzer('6ba88516f54936e6db7e6813dfe36f21928181b9b19def5d6ce66a489dd4ae96')
+    analyzer = DomainAnalyzer()
     df = analyzer.process_domains("false_positives/highest_shap.txt")
     analyzer.generate_report(df, 'false_positives/FP_check.pdf')
+    analyzer.client.close()
