93c4937ca71e295cd732bb45d20339b59c4d4666
survivecz <poucpetr11@seznam.cz>
2023-11-11 11:41:19 +0100

    undersampling script

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     # MongoDB setup
-    MONGO_URI = getenv("DR_MONGO_URI", "mongodb://localhost:27017/")
+    MONGO_URI = getenv("DR_MONGO_URI", "mongodb://root:doktorkolektor@feta3.fit.vutbr.cz:27017/")
     MONGO_DB = "drdb"
     # Collections to process by loader - {label: collection_name}
     COLLECTIONS = {
@@ -15,5 +15,5 @@ class Config:
         # "benign_2307": "benign_2307"
         # "benign_cesnet_union_2307": "benign_cesnet_union_2307"
         # "malware":"malware"
-        "misp_2307": "misp_2307",
+        "misp_2310": "misp_2310",
     }
