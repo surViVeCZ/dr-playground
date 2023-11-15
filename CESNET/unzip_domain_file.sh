72330296629a27b932c3f38672706a9fd9904f46
survivecz <poucpetr11@seznam.cz>
2023-11-15 17:27:55 +0100

    unzip script, sampling script, txt mode for VT

--- CESNET/unzip_domain_file.sh
+++ CESNET/unzip_domain_file.sh
@@ -4,7 +4,7 @@
 # and the Python script 'append_to_excel.py' is also in the 'CESNET' folder.
 
 # The name of the Excel file to append domains to
-excel_file="CESNET_domains.xlsx"
+parquet_file="CESNET_domains.parquet"
 
 # Navigate to the data folder where the .gz files are located
 cd data
@@ -17,7 +17,7 @@ for file in *.gz; do
 
     # Call the Python script to append the domains to the Excel file
     # Using '../' to go up one level from the 'data' folder to the 'CESNET' folder where the Python script is located
-    /usr/bin/python3 ../append_to_excel.py "$csv_file" "../$excel_file"
+    /usr/bin/python3 ../append_to_parquet.py "$csv_file" "../$parquet_file"
 
     # Remove the unzipped CSV file
     rm "$csv_file"
